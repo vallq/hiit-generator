513b645a5bfcc60750d8f0767df55183d679a154
Valerie Lui <vallq@users.noreply.github.com>
2020-04-05 19:11:02 +0800

    add workout card component

---
+++ src/components/WorkoutCard.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./WorkoutCard.css";
+import { uuid } from "uuidv4";
+
+function WorkoutCard(data) {
+  const { exercises, name, duration, date } = data;
+  const key = uuid();
+
+  return (
+    <div key={key} className="workout-card">
+      <h2>{name}</h2>
+      <h3>Workout</h3>
+      <div className="workout-card__exercises">
+        {exercises.map(exercise => (
+          <li>{exercise}</li>
+        ))}
+      </div>
+      <h4>duration: {duration / 60} min</h4>
+      <h4>{date}</h4>
+    </div>
+  );
+}
+
+export default WorkoutCard;
